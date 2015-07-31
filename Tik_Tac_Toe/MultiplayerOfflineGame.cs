c922deb091160e497641cecc39f4609b896295f4
Janaka Chathuranga <bjchathuranga@gmail.com>
2015-07-31 15:52:53 +0530

    Added multiplayer offline

---
+++ Tik_Tac_Toe/MultiplayerOfflineGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace Tik_Tac_Toe
+{
+    public class MultiplayerOfflineGame:Game
+    {
+
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Game));
+        private int nextPlayerId; // 1 or -1
+        public MultiplayerOfflineGame():base()
+        {
+            logger.Info("creating a multiplayer game");
+             nextPlayerId = 1;
+        }
+
+        public override bool updateMove(int row, int col)
+        {
+            if (table[row, col] != 0)
+            {
+                logger.Warn("Requested a imposible move");
+                return false;
+            }
+
+            updateMove(row, col, nextPlayerId);
+            nextPlayerId *= (-1);
+            stateMessage = "Game is started";
+            callUpdate();
+            return true;
+        }
+
+        public override void reset(bool full)
+        {
+            base.reset(full);
+            nextPlayerId = (new Random()).Next(0, 2);
+            if (nextPlayerId == 0) nextPlayerId=-1;
+        }
+    }
+}
---
+++ Tik_Tac_Toe_Test/Test_Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tik_Tac_Toe;
+
+namespace Tik_Tac_Toe_Test
+{
+    
+    [TestClass]
+    public class Test_Game //to Test the logical game
+    {
+        [TestMethod]
+        public void CreateGame()
+        {
+            Game game = new MultiplayerOfflineGame();
+            Assert.AreEqual(0, game.getPlayer(-1).points);
+            Assert.AreEqual(0, game.getPlayer(1).points);
+        }
+    }
+}
