c32b87a25e3201cbbef966f584c21f780fd49f96
Janaka Chathuranga <bjchathuranga@gmail.com>
2015-07-31 16:40:50 +0530

    OnlineGame class added

---
+++ Tik_Tac_Toe/MultiplayerOnlineGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace Tik_Tac_Toe
+{
+    class MultiplayerOnlineGame:Game
+    {
+        //logger
+        private static readonly ILog logger = LogManager.GetLogger(typeof(SinglePlayerGame));
+
+        public override bool updateMove(int row, int col)
+        {
+            if (table[row, col] != 0)
+            {
+                logger.Warn("Requested a imposible move");
+                return false;
+            }
+            return false;
+        }
+    }
+}
